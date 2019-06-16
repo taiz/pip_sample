f390d253935553759394dedf16f0a4929bf4b37d
Taiji.Miyabe <taiz7715@gmail.com>
2019-06-16 12:19:58 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="libhollow",
+    version="1.0",
+    author="momokan",
+    author_email="momokan@example.com",
+    description="libhollow is my own python package",
+    long_description="The halloween party has gone over...",
+    long_description_content_type="text/markdown",
+    url="https://blog.chocolapod.net/momokan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7.14",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
